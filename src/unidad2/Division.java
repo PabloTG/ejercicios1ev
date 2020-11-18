676e6d80036f57b04750f3a896e84e07b68df366
PabloT <pablotga82@educastur.es>
2020-11-18 11:32:58 +0100

    Ejercicio 1 de la unidad 2 resuelto

---
+++ src/unidad2/Division.java
@@ -0,0 +1,21 @@
+package unidad2;
+
+import java.text.DecimalFormat;
+
+public class Division {
+
+	public static void main(String[] args) {
+//declaro dividendo y divisor y los inicializo a los números que me interesan
+double dividendo = 1234, divisor = 532;
+//realizo la división
+double division = dividendo / divisor;
+/*muestro el resultado de la división ocupando como mínimo quince caracteres, 
+ sólo dos de ellos para la parte decimal, utilizando la clase DecimalFormat 
+ (https://docs.oracle.com/javase/7/docs/api/java/text/DecimalFormat.html).
+ Se mostrarán dieciséis caracteres incluyendo la coma: trece para la parte 
+ numérica, la coma, y dos para la parte decimal. */
+DecimalFormat mostrar = new DecimalFormat("0000000000000.00");
+System.out.println(mostrar.format(division));
+	}
+
+}
